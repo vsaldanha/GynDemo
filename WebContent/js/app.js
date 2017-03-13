35a942f7de761bd600d3c18170c742ced5ad2219
Vanita <Vanita>
2017-03-14 00:52:14 +0530

    Tabular forms development

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -20,80 +20,35 @@ routingApp.config([ '$routeProvider', '$locationProvider',
 			})
 		} ]);
 
-routingApp.controller("uigridCtrl", function($scope) {
+routingApp.controller("uigridCtrl", function($scope, $http) {
 	$scope.gridOptions = {
 		enableFiltering : true,
 		enablePagination: true,
+		data:'data',
 		paginationPageSizes: [5, 10, 15],
 		paginationPageSize: 5,
 		onRegisterApi : function(gridApi) {
 			$scope.grid1Api = gridApi;
 		}
 	};
-	$scope.users = [ {
-		ur_number : 154104,
-		surname : 'Bortignon',
-		given_name : 'Maureen Joy',
-		date_of_birth : '21/10/1951',
-		first_presented_diagnosis : 'Uterus'
-	}, {
-		ur_number : 158875,
-		surname : 'Cumming',
-		given_name : 'Tahnee',
-		date_of_birth : '09/02/1976',
-		first_presented_diagnosis : 'Cervix'
-	}, {
-		ur_number : 151233,
-		surname : 'Simpkin',
-		given_name : 'Dorothy May',
-		date_of_birth : '01/05/1974',
-		first_presented_diagnosis : 'Vulva'
-	}, {
-		ur_number : 167890,
-		surname : 'Walters',
-		given_name : 'Margaret Anne',
-		date_of_birth : '10/10/1988',
-		first_presented_diagnosis : 'Ovary'
-	}, {
-		ur_number : 134569,
-		surname : 'De Bruijne',
-		given_name : 'Elizabeth',
-		date_of_birth : '16/04/1969',
-		first_presented_diagnosis : 'Fallopian Tube'
-	}, {
-		ur_number : 151109,
-		surname : 'Poland',
-		given_name : 'Rose',
-		date_of_birth : '28/03/1959',
-		first_presented_diagnosis : 'Uterus'
-	}, {
-		ur_number : 153567,
-		surname : 'Bishop',
-		given_name : 'Mary',
-		date_of_birth : '09/02/1976',
-		first_presented_diagnosis : 'Cervix'
-	}, {
-		ur_number : 134657,
-		surname : 'Lees',
-		given_name : 'Alison',
-		date_of_birth : '01/05/1974',
-		first_presented_diagnosis : 'Vulva'
-	}, {
-		ur_number : 166752,
-		surname : 'Vaega',
-		given_name : 'Voice',
-		date_of_birth : '30/09/1980',
-		first_presented_diagnosis : 'Ovary'
-	}, {
-		ur_number : 145678,
-		surname : 'Jacobs',
-		given_name : 'Robyn',
-		date_of_birth : '16/03/1986',
-		first_presented_diagnosis : 'Ovary'
-	},
+	
+	$scope.gridOptions.columnDefs = [
+	              {field: 'ur_number' }, 
+	              {field: 'surname' }, 
+	              {field: 'given_name' },
+	              {field: 'date_of_birth' },
+	              {field: 'first_presented_diagnosis' }];
+	
+	$http.get('Gyn/mockJSON/mock.json').then(successCallback, errorCallback);
+	function successCallback(data){
+		console.log(data.data);
+		$scope.data = data.data;
+	}
+	function errorCallback(error){
+	    console.log("AJAX call error");
+	}
 
-	];
-	$scope.gridOptions.data = $scope.users;
+	
 });
 
 /* Controller for Homepage */
@@ -103,8 +58,7 @@ routingApp.controller('HomeController', function($scope) {
 });
 
 /* Controller for Grid */
-routingApp
-		.controller(
+routingApp.controller(
 				'GridController',
 				function($scope) {
 					console.log("grid controller executed");
