fad0782074f3a37acf392aedcebc256b33bc4d7c
Vanita <Vanita>
2017-03-14 10:07:55 +0530

    Development 2

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,4 +1,5 @@
-var routingApp = angular.module('routingApp', [ 'ngRoute', 'ui.grid', 'ui.grid.pagination' ]);
+var routingApp = angular.module('routingApp', [ 'ngRoute', 'ui.grid',
+		'ui.grid.pagination' ]);
 
 console.log("Entered app.js")
 
@@ -11,6 +12,9 @@ routingApp.config([ '$routeProvider', '$locationProvider',
 			}).when('/grid', {
 				templateUrl : 'Gyn/pages/grid.html',
 				controller : 'GridController'
+			}).when('/tabs', {
+				templateUrl : 'Gyn/pages/tabular_forms.html',
+				controller : 'TabController'
 			}).otherwise({
 				redirectTo : '/index'
 			});
@@ -20,36 +24,45 @@ routingApp.config([ '$routeProvider', '$locationProvider',
 			})
 		} ]);
 
-routingApp.controller("uigridCtrl", function($scope, $http) {
-	$scope.gridOptions = {
-		enableFiltering : true,
-		enablePagination: true,
-		data:'data',
-		paginationPageSizes: [5, 10, 15],
-		paginationPageSize: 5,
-		onRegisterApi : function(gridApi) {
-			$scope.grid1Api = gridApi;
-		}
-	};
-	
-	$scope.gridOptions.columnDefs = [
-	              {field: 'ur_number' }, 
-	              {field: 'surname' }, 
-	              {field: 'given_name' },
-	              {field: 'date_of_birth' },
-	              {field: 'first_presented_diagnosis' }];
-	
-	$http.get('Gyn/mockJSON/mock.json').then(successCallback, errorCallback);
-	function successCallback(data){
-		console.log(data.data);
-		$scope.data = data.data;
-	}
-	function errorCallback(error){
-	    console.log("AJAX call error");
-	}
+routingApp.controller(
+				"uigridCtrl",
+				function($scope, $http) {
+					$scope.gridOptions = {
+						enableFiltering : true,
+						enablePagination : true,
+						data : 'data',
+						paginationPageSizes : [ 5, 10, 15 ],
+						paginationPageSize : 5,
+						onRegisterApi : function(gridApi) {
+							$scope.grid1Api = gridApi;
+						}
+					};
 
-	
-});
+					$scope.gridOptions.columnDefs = [
+							{
+								field : 'ur_number',
+								cellTemplate : '<div><a href="/tabs">{{row.entity.ur_number}}</a></div>'
+							}, {
+								field : 'surname'
+							}, {
+								field : 'given_name'
+							}, {
+								field : 'date_of_birth'
+							}, {
+								field : 'first_presented_diagnosis'
+							} ];
+
+					$http.get('Gyn/mockJSON/mock.json').then(successCallback,
+							errorCallback);
+					function successCallback(data) {
+						console.log(data.data);
+						$scope.data = data.data;
+					}
+					function errorCallback(error) {
+						console.log("AJAX call error");
+					}
+
+				});
 
 /* Controller for Homepage */
 routingApp.controller('HomeController', function($scope) {
@@ -63,4 +76,22 @@ routingApp.controller(
 				function($scope) {
 					console.log("grid controller executed");
 					$scope.message = 'Message from Controller B. Using Routing showing in Template B.';
+				});
+
+/* Controller for displaying tabular forms */
+routingApp.controller(
+				'TabController',
+				function($scope, $window, $http) {
+					console.log("tab controller executed");
+					
+					$http.get('Gyn/mockJSON/PatientDetails.json').then(successCallback,
+							errorCallback);
+					function successCallback(data) {
+						console.log(data.data);
+						$scope.data = data.data;
+					}
+					function errorCallback(error) {
+						console.log("AJAX call error");
+					}
+					$window.location.href="http://localhost:8080/Gyn/pages/tabular_forms.html";
 				});
